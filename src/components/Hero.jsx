79cdfdcbb087082a1542719ef4fab29d4e156a3b
DC-21 <cholahkuboko@gmail.com>
2023-06-19 16:49:48 +0200

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import cyber from '../images/cyber.jpg'
 const Hero = () => {
   return (
     <div className='w-full h-screen flex flex-wrap py-6 bg-[#2d2628] px-8'>
-      <div className='w-1/2 mt-16 flex'>
+      <div className='w-1/2 mt-16 flex flex-col'>
         <p className='text-white'>What is Cyber Security ?</p>
         <h1 className='text-white text-5xl'>The Future Of Cyber Security Is Here</h1>
       </div>
