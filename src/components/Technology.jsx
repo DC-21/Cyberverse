44ddd231cf15917e064c631f3fbdabaf80a42e36
DC-21 <cholahkuboko@gmail.com>
2023-06-19 20:44:29 +0200

    first commit

--- src/components/Technology.jsx
+++ src/components/Technology.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Technology = () => {
   return (
-    <div className='w-full h-screen flex flex-wrap py-6 bg-[#1f2123] px-8'>
+    <div className='w-full h-screen flex flex-col flex-wrap py-6 bg-[#1f2123] px-8'>
         <div>
             <h1 className='text-5xl text-white mt-12'>The Future Of Cyber Security Is Here.</h1>
         </div>
-        <div>
-            <div></div>
-            <div></div>
-            <div></div>
+        <div className='mt-10'>
+            <div className='w-1/3 h-[450px] border border-blue-50'></div>
+            <div className='w-1/3 h-[450px] border border-blue-50'></div>
+            <div className='w-1/3 h-[450px] border border-blue-50'></div>
         </div>
       
     </div>
