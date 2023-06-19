0fb341e0e615b6f14cce96b7abe558fa7d3bd203
DC-21 <cholahkuboko@gmail.com>
2023-06-19 20:46:05 +0200

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
         <p className='text-white pl-14 mt-8'>Looking forward to the most advanced modes of <br/>security of massive and confidential information<br/> of all stake  holders accross the globe. Securing <br/>the web is our top priority... See you soon ?</p>
         <div className='text-white pl-14 mt-8 gap-6 flex'>
             <button className='bg-[#fdbe0e] py-2 px-3 rounded text-black'>See More</button>
-            <button className='bg-[#44423d] py-2 border-b-[2px] border-slate-500 px-3 rounded'>Download Resource</button>
+            <button className='bg-[#44423d] py-2 border border-slate-500 px-3 rounded'>Download Resource</button>
         </div>
       </div>
       <div className='w-1/2 flex '>
--- src/components/Technology.jsx
+++ src/components/Technology.jsx
@@ -6,7 +6,7 @@ const Technology = () => {
         <div>
             <h1 className='text-5xl text-white mt-12'>The Future Of Cyber Security Is Here.</h1>
         </div>
-        <div className='mt-10'>
+        <div className='mt-10 flex'>
             <div className='w-1/3 h-[450px] border border-blue-50'></div>
             <div className='w-1/3 h-[450px] border border-blue-50'></div>
             <div className='w-1/3 h-[450px] border border-blue-50'></div>
