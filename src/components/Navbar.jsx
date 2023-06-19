491c690dffd3802e4a376a9d1cdf77cd08474a73
DC-21 <cholahkuboko@gmail.com>
2023-06-19 16:27:38 +0200

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,10 @@ const Navbar = () => {
   };
 
   return (
-    <div className="bg-[#1f2123] text-white flex w-full px-4 py-6 justify-between">
+    <div className="bg-[#1f2123] text-white flex w-full px-4 py-6 items-center justify-between">
       <div className="flex gap-8">
         <a href="">Tokenize</a>
-        <ul className="flex gap-8">
+        <ul className="md:flex hidden gap-8">
           <li>
             <a>What is Tokenize?</a>
           </li>
@@ -31,7 +31,7 @@ const Navbar = () => {
           </li>
         </ul>
       </div>
-      <div className="flex gap-6 items-center">
+      <div className="md:flex hidden gap-6 items-center">
         <a>
           <svg
             viewBox="0 0 1024 1024"
@@ -44,14 +44,14 @@ const Navbar = () => {
         </a>
         <button>Log In</button>
       </div>
-      <div>
-        <button onClick={handleMenu}>
+      <div className={`md:hidden ${open ? "right-3" : "right-3"} absolute`}>
+        <button  onClick={handleMenu}>
           {open ? (
             <svg
               viewBox="0 0 470 1000"
               fill="currentColor"
-              height="2em"
-              width="2em"
+              height="1.5em"
+              width="1.5em"
             >
               <path d="M452 656c12 12 18 26.333 18 43s-6 31-18 43c-12 10.667-26.333 16-43 16s-31-5.333-43-16L234 590 102 742c-12 10.667-26.333 16-43 16s-31-5.333-43-16C5.333 730 0 715.667 0 699s5.333-31 16-43l138-156L16 342C5.333 330 0 315.667 0 299s5.333-31 16-43c12-10.667 26.333-16 43-16s31 5.333 43 16l132 152 132-152c12-10.667 26.333-16 43-16s31 5.333 43 16c12 12 18 26.333 18 43s-6 31-18 43L314 500l138 156" />
             </svg>
@@ -59,12 +59,30 @@ const Navbar = () => {
             <svg
               viewBox="0 0 700 1000"
               fill="currentColor"
-              height="2em"
-              width="2em"
+              height="1.5em"
+              width="1.5em"
             >
               <path d="M650 450c14.667 0 26.667 5 36 15 9.333 10 14 21.667 14 35 0 13.333-5 25-15 35s-21.667 15-35 15H50c-13.333 0-25-5-35-15S0 513.333 0 500c0-13.333 4.667-25 14-35s21.333-15 36-15h600M50 350c-13.333 0-25-5-35-15S0 313.333 0 300c0-13.333 4.667-25 14-35s21.333-15 36-15h600c14.667 0 26.667 5 36 15 9.333 10 14 21.667 14 35 0 13.333-5 25-15 35s-21.667 15-35 15H50m600 300c14.667 0 26.667 5 36 15 9.333 10 14 21.667 14 35 0 13.333-5 25-15 35s-21.667 15-35 15H50c-13.333 0-25-5-35-15S0 713.333 0 700c0-13.333 4.667-25 14-35s21.333-15 36-15h600" />
             </svg>
           )}
+          {open ? 
+        <ul className="absolute right-0 shadow-2xl items-center z-50 top-[3.7em] w-[6em] py-2 gap-2 flex flex-col text-left h-[11em] rounded bg-[#1f2123]">
+          <li>
+            <a>Platform</a>
+          </li>
+          <li>
+            <a>Solutions</a>
+          </li>
+          <li>
+            <a>Reference</a>
+          </li>
+          <li>
+            <a>About</a>
+          </li>
+          <li>
+            <a>Contact</a>
+          </li>
+        </ul>:null }
         </button>
       </div>
     </div>
