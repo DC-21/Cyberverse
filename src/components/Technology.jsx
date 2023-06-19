b8d55ec74a35f27581ddb20b09b1684b678df21a
DC-21 <cholahkuboko@gmail.com>
2023-06-19 20:51:07 +0200

    first commit

--- src/components/Technology.jsx
+++ src/components/Technology.jsx
@@ -7,11 +7,16 @@ const Technology = () => {
             <h1 className='text-5xl text-white mt-12'>The Future Of Cyber Security Is Here.</h1>
         </div>
         <div className='mt-10 flex'>
-            <div className='w-1/3 h-[450px] border border-blue-50'></div>
-            <div className='w-1/3 h-[450px] border border-blue-50'></div>
-            <div className='w-1/3 h-[450px] border border-blue-50'></div>
+            <div className='w-1/3 h-[450px] border border-blue-50 items-center justify-between'>
+                <h1 className='text-center text-white mt-6 text-3xl px-4'>Secure Data Collection</h1>
+            </div>
+            <div className='w-1/3 h-[450px] border border-blue-50'>
+                <h1 className='text-center text-white mt-6 text-3xl px-4'>Secure Data Transmission</h1>
+            </div>
+            <div className='w-1/3 h-[450px] border border-blue-50'>
+                <h1 className='text-center text-white mt-6 text-3xl px-4'>Other Products & Integrations</h1>
+            </div>
         </div>
-      
     </div>
   )
 }
