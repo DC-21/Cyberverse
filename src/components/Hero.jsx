d1efa5161685ad5901fff115a9c963f52b950d05
DC-21 <cholahkuboko@gmail.com>
2023-06-19 16:45:28 +0200

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import cyber from '../images/cyber.jpg'
 
 const Hero = () => {
   return (
-    <div className='w-full h-screen flex justify-center items-center bg-[#2d2628] px-8'>
+    <div className='w-full h-screen flex py-6 bg-[#2d2628] px-8'>
       <div className='w-1/2'>
         <p>What is Tokenization ?</p>
         <h1 className='text-white text-5xl'>The Future Of Cyber Security Is Here</h1>
