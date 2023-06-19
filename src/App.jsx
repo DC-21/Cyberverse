e34176c3baec385a3ab502a189a8b2aa698a3e73
DC-21 <cholahkuboko@gmail.com>
2023-06-19 15:31:35 +0200

    Upload HTML file

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <Navbar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <a href=''>Tokenize</a>
+        <ul>
+            <li>
+                <a></a>
+            </li>
+            <li>
+                <a></a>
+            </li>
+            <li>
+                <a></a>
+            </li>
+            <li>
+                <a></a>
+            </li>
+            <li>
+                <a></a>
+            </li>
+            <li>
+                <a></a>
+            </li>
+        </ul>
+      </div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Navbar
