7022bb39672e23baffabbab993eb85e4349d04e5
DC-21 <cholahkuboko@gmail.com>
2023-06-20 13:42:42 +0200

    first commit

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -319,7 +319,161 @@ const About = () => {
             </li>
           </ul>
         </div>
-        <div className="w-1/3 h-full rounded border border-blue-50"></div>
+        <div className="w-1/3 h-full rounded border border-blue-50">
+        <h1 className="text-center py-6 text-2xl text-white">Web Development Services</h1>
+          <ul className="px-4 text-indigo-200 text-l">
+            <li className="items-center flex gap-3 py-4">
+              <svg
+                viewBox="0 0 512 512"
+                fill="currentColor"
+                height="1em"
+                width="1em"
+              >
+                <path
+                  fill="none"
+                  stroke="currentColor"
+                  strokeMiterlimit={10}
+                  strokeWidth={32}
+                  d="M64 256c0 106 86 192 192 192s192-86 192-192S362 64 256 64 64 150 64 256z"
+                />
+                <path
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={32}
+                  d="M216 352l96-96-96-96"
+                />
+              </svg>
+              <a>Full-Stack Development</a>
+            </li>
+            <li className="items-center flex gap-3 py-4">
+              <svg
+                viewBox="0 0 512 512"
+                fill="currentColor"
+                height="1em"
+                width="1em"
+              >
+                <path
+                  fill="none"
+                  stroke="currentColor"
+                  strokeMiterlimit={10}
+                  strokeWidth={32}
+                  d="M64 256c0 106 86 192 192 192s192-86 192-192S362 64 256 64 64 150 64 256z"
+                />
+                <path
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={32}
+                  d="M216 352l96-96-96-96"
+                />
+              </svg>
+              <a>Mobile App Design</a>
+            </li>
+            <li className="items-center flex gap-3 py-4">
+              <svg
+                viewBox="0 0 512 512"
+                fill="currentColor"
+                height="1em"
+                width="1em"
+              >
+                <path
+                  fill="none"
+                  stroke="currentColor"
+                  strokeMiterlimit={10}
+                  strokeWidth={32}
+                  d="M64 256c0 106 86 192 192 192s192-86 192-192S362 64 256 64 64 150 64 256z"
+                />
+                <path
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={32}
+                  d="M216 352l96-96-96-96"
+                />
+              </svg>
+              <a>Print Design</a>
+            </li>
+            <li className="items-center flex gap-3 py-4">
+              <svg
+                viewBox="0 0 512 512"
+                fill="currentColor"
+                height="1em"
+                width="1em"
+              >
+                <path
+                  fill="none"
+                  stroke="currentColor"
+                  strokeMiterlimit={10}
+                  strokeWidth={32}
+                  d="M64 256c0 106 86 192 192 192s192-86 192-192S362 64 256 64 64 150 64 256z"
+                />
+                <path
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={32}
+                  d="M216 352l96-96-96-96"
+                />
+              </svg>
+              <a>Branding And Identity Design</a>
+            </li>
+            <li className="items-center flex gap-3 py-4">
+              <svg
+                viewBox="0 0 512 512"
+                fill="currentColor"
+                height="1em"
+                width="1em"
+              >
+                <path
+                  fill="none"
+                  stroke="currentColor"
+                  strokeMiterlimit={10}
+                  strokeWidth={32}
+                  d="M64 256c0 106 86 192 192 192s192-86 192-192S362 64 256 64 64 150 64 256z"
+                />
+                <path
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={32}
+                  d="M216 352l96-96-96-96"
+                />
+              </svg>
+              <a>Advertising And Market Design</a>
+            </li>
+            <li className="items-center flex gap-3 py-4">
+              <svg
+                viewBox="0 0 512 512"
+                fill="currentColor"
+                height="1em"
+                width="1em"
+              >
+                <path
+                  fill="none"
+                  stroke="currentColor"
+                  strokeMiterlimit={10}
+                  strokeWidth={32}
+                  d="M64 256c0 106 86 192 192 192s192-86 192-192S362 64 256 64 64 150 64 256z"
+                />
+                <path
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={32}
+                  d="M216 352l96-96-96-96"
+                />
+              </svg>
+              <a>Data Visualization</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
