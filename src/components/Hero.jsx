4efd01366f59d35f75001442abb0ee89b5eead6a
DC-21 <cholahkuboko@gmail.com>
2023-06-20 11:00:06 +0200

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,27 @@
-import image from '../images/c.png'
+import React from 'react';
+import { Player } from '@lottiefiles/react-lottie-player';
 
 const Hero = () => {
   return (
-    <div className='h-screen w-full  px-8' style={{ backgroundImage:`url(${image})` }}>
-      <div className='w-full h-full items-center justify-center'>
+    <div className='h-screen w-full px-8 relative'>
+      <Player
+        autoplay
+        loop
+        src='https://assets2.lottiefiles.com/packages/lf20_kx3p7bbx.json'
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          zIndex: -1,
+        }}
+      />
+      <div className='w-full h-full flex items-center justify-center'>
         <h1 className='text-4xl py-20 text-white text-center pt-20'>The Future Of Cyber Security Is Here!!</h1>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
