e780858598d8692118913cb4c92afb351c2fe77f
DC-21 <cholahkuboko@gmail.com>
2023-06-20 10:48:06 +0200

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className='h-screen w-full  px-8' style={{ backgroundImage:`url(${image})` }}>
       <div className='w-full h-full items-center justify-center'>
-        <h1 className='text-4xl py-20 text-white text-center pt-20'>The Future Of Cyber Security Is Here</h1>
+        <h1 className='text-4xl py-20 text-white text-center pt-20'>The Future Of Cyber Security Is Here!!</h1>
       </div>
     </div>
   )
