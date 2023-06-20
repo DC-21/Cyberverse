391a973102294c73f6541e6a5a0a4d12bdcf0e79
DC-21 <cholahkuboko@gmail.com>
2023-06-20 14:19:10 +0200

    first commit

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="flex flex-wrap w-full md:h-screen h-auto px-8 py-6 bg-[#381495] gap-6">
-      <h1 className="text-white text-4xl py-10 w-full text-center">
+    <div className="flex flex-wrap w-full md:h-screen h-auto px-8 py-6 bg-[#381495]">
+      <h1 className="text-white text-4xl py-6 w-full text-center">
         Our Services
       </h1>
       <div className="w-full md:h-[450px] h-auto flex md:flex-row flex-col gap-4">
