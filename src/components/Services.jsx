861e4ed5f1bd5bb787ed20ea59e49a230a5ca410
DC-21 <cholahkuboko@gmail.com>
2023-06-20 12:32:25 +0200

    first commit

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='w-full h-screen bg-blue-800'>
+    <div className='flex flex-wrap w-full h-screen bg-[#381495]'>
       <h1 className='text-white text-4xl py-10 w-full text-center'>Our Services</h1>
     </div>
   )
