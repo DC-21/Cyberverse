cce8e2bc92ad9a52f7732dc41be82714a6587621
DC-21 <cholahkuboko@gmail.com>
2023-06-20 10:45:46 +0200

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import image from '../images/c.png'
 
 const Hero = () => {
   return (
-    <div>
-      
+    <div className='h-screen w-full' style={{ backgroundImage:`url(${image})` }}>
+      <div className='w-full h-full items-center justify-center'>
+        <h1 className='text-4xl text-white text-center pt-20'>Hello world</h1>
+      </div>
     </div>
   )
 }
