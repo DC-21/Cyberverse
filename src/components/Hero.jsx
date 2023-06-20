c968dec96f69cac250379c2e658a354178842722
DC-21 <cholahkuboko@gmail.com>
2023-06-20 10:51:28 +0200

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import image from '../images/c.png'
 
 const Hero = () => {
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,16 +12,16 @@ const Navbar = () => {
         <a href="">Cyber Wave</a>
         <ul className="md:flex hidden gap-8">
           <li>
-            <a>Platform</a>
-          </li>
-          <li>
-            <a>Solutions</a>
-          </li>
-          <li>
-            <a>Reference</a>
+            <a>Home</a>
           </li>
           <li>
             <a>About</a>
+          </li>
+          <li>
+            <a>Services</a>
+          </li>
+          <li>
+            <a>Pricing</a>
           </li>
           <li>
             <a>Contact</a>
@@ -29,16 +29,7 @@ const Navbar = () => {
         </ul>
       </div>
       <div className="md:flex hidden gap-6 items-center">
-        <a>
-          <svg
-            viewBox="0 0 1024 1024"
-            fill="currentColor"
-            height="1em"
-            width="1em"
-          >
-            <path d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0011.6 0l43.6-43.5a8.2 8.2 0 000-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z" />
-          </svg>
-        </a>
+       <button>Sign Up</button>
         <button>Log In</button>
       </div>
       <div className={`md:hidden ${open ? "right-3" : "right-3"} absolute`}>
