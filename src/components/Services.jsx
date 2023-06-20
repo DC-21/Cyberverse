03d0965002426f87b4fdf0f0b9f11d814eceae28
DC-21 <cholahkuboko@gmail.com>
2023-06-20 12:53:30 +0200

    first commit

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -5,7 +5,51 @@ const About = () => {
     <div className='flex flex-wrap w-full h-screen px-8 py-6 bg-[#381495] gap-6'>
       <h1 className='text-white text-4xl py-10 w-full text-center'>Our Services</h1>
       <div className='w-full h-[450px] flex flex-row gap-4'>
-      <div className='w-1/3 h-full rounded border border-blue-50'></div>
+      <div className='w-1/3 h-full rounded border border-blue-50'>
+        <h1 className='text-center py-6 text-2xl text-white'>Cyber Security Services</h1>
+        <ul className='px-4 text-indigo-200 text-l'>
+          <li className='items-center flex gap-3 py-4'>
+          <svg
+      viewBox="0 0 512 512"
+      fill="currentColor"
+      height="1em"
+      width="1em"
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeMiterlimit={10}
+        strokeWidth={32}
+        d="M64 256c0 106 86 192 192 192s192-86 192-192S362 64 256 64 64 150 64 256z"
+      />
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={32}
+        d="M216 352l96-96-96-96"
+      />
+    </svg>
+            <a>Security Awareness Training</a>
+          </li>
+          <li className='py-4'>
+            <a>Security Incident Response</a>
+          </li>
+          <li className='py-4'>
+            <a>Security Consulting and Risk Assessment</a>
+          </li>
+          <li className='py-4'>
+            <a>Network Security and Firewall Management</a>
+          </li>
+          <li className='py-4'>
+            <a>Website Penetration Testing</a>
+          </li>
+          <li className='py-4'>
+            <a>Cloud Security</a>
+          </li>
+        </ul>
+      </div>
       <div className='w-1/3 h-full rounded border border-blue-50'></div>
       <div className='w-1/3 h-full rounded border border-blue-50'></div>
       </div>
