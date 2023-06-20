1701bf8b5797ac0feea6adce5174024f308748d4
DC-21 <cholahkuboko@gmail.com>
2023-06-20 12:30:47 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
-import About from './components/About'
+import Services from './components/Services'
 
 function App() {
 
@@ -9,7 +9,7 @@ function App() {
     <>
      <Navbar/>
      <Hero/>
-     <About/>
+     <Services/>
     </>
   )
 }
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <div className='w-full h-screen bg-blue-800'>
-      <h1>About Us</h1>
+      <h1 className='text-white text-4xl py-10 w-full text-center'>Our Services</h1>
     </div>
   )
 }
