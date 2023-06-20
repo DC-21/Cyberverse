25397978bc96e8e0a038d6b803ed898989b7b2c2
DC-21 <cholahkuboko@gmail.com>
2023-06-20 16:15:41 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Services from './components/Services'
 import Pricing from './components/Pricing'
+import Team from './components/Team'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
      <Hero/>
      <Services/>
      <Pricing/>
+     <Team/>
     </>
   )
 }
